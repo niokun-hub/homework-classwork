eedfc29db6622da299126900a2dfd0c5b67c4e80
KOMP-7 <hikikomorikkai@gmail.com>
2026-03-25 09:21:14 +1000

    сделал проверку функций в masks.py

--- Camel_case/bank_project/src/masks.py
+++ Camel_case/bank_project/src/masks.py
@@ -12,4 +12,15 @@ def get_mask_account(account_number: str) -> str:
     return f"**{last_digits}"
 
 
+if __name__ == "__main__":
+    print("ТЕСТИРОВАНИЕ ФУНКЦИЙ МАСКИРОВКИ")
 
+    print("\n1. МАСКИРОВКА КАРТЫ")
+    card_number = "7000792289606361"
+    print(f"Входной номер: {card_number}")
+    print(f"Результат:     {get_mask_card_number(card_number)}")
+
+    print("\n2. МАСКИРОВКА СЧЕТА")
+    account_number = "73654108430135874305"
+    print(f"Входной номер: {account_number}")
+    print(f"Результат:     {get_mask_account(account_number)}")
