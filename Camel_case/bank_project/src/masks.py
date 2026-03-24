221f8bc0527c7ee5a6f582efd0d65238ba34ce90
KOMP-7 <hikikomorikkai@gmail.com>
2026-03-25 09:20:49 +1000

    сделал 2 функции в masks.py

---
+++ Camel_case/bank_project/src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(card_number: str) -> str:
+
+    first_part = card_number[:4]
+    second_part = card_number[4:6]
+    last_part = card_number[-4:]
+    return f"{first_part} {second_part}** **** {last_part}"
+
+
+def get_mask_account(account_number: str) -> str:
+    last_digits = account_number[-4:]
+
+    return f"**{last_digits}"
+
+
+
